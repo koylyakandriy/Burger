5470e9b10163998fd4e614ea0d9661e36e2ecc73
Travis CI <travis@travis-ci.org>
2019-07-08 07:56:12 +0300

    add saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -24,51 +22,30 @@ export const authFail = error => {
 };
 
 export const logout = () => {
-	localStorage.removeItem('token');
-	localStorage.removeItem('expirationDate');
-	localStorage.removeItem('userId');
+	return {
+		type: actionTypes.AUTH_INITIATE_LOGOUT,
+	};
+};
+
+export const logoutSucceed = () => {
 	return {
 		type: actionTypes.AUTH_LOGOUT,
 	};
 };
 
 export const checkAuthTimeout = expirationTime => {
-	return dispatch => {
-		setTimeout(() => {
-			dispatch(logout());
-		}, expirationTime * 1000);
+	return {
+		type: actionTypes.AUTH_CHECK_TIMEOUT,
+		expirationTime: expirationTime,
 	};
 };
 
 export const auth = (email, password, isSignup) => {
-	return dispatch => {
-		dispatch(authStart());
-		const authData = {
-			email: email,
-			password: password,
-			returnSecureToken: true,
-		};
-		let url =
-			'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDPbBZUpUcOUJvHggbYr3qM_WqwPxpC18Y';
-		if (!isSignup) {
-			url =
-				'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDPbBZUpUcOUJvHggbYr3qM_WqwPxpC18Y';
-		}
-		axios
-			.post(url, authData)
-			.then(response => {
-				const expirationDate = new Date(
-					new Date().getTime() + response.data.expiresIn * 1000,
-				);
-				localStorage.setItem('token', response.data.idToken);
-				localStorage.setItem('expirationDate', expirationDate);
-				localStorage.setItem('userId', response.data.localId);
-				dispatch(authSuccess(response.data.idToken, response.data.localId));
-				dispatch(checkAuthTimeout(response.data.expiresIn));
-			})
-			.catch(err => {
-				dispatch(authFail(err.response.data.error));
-			});
+	return {
+		type: actionTypes.AUTH_USER,
+		email: email,
+		password: password,
+		isSignup: isSignup,
 	};
 };
 
